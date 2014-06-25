b0761e030dd973f3c5dec8a3972554a6b982bf2d
Greg Scott <i@gregoryjscott.com>
2014-06-25 08:06:29 -0600

    Setup grunt-gh-pages

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    'gh-pages': {
+      options: {
+        base: '_site'
+      },
+      src: ['**']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+};
